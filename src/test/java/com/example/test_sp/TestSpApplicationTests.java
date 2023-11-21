19f996ad354bf63c9dc7a4531225bc4cb0b65969
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2023-11-21 09:26:40 +0200

    test

--- src/test/java/com/example/test_sp/TestSpApplicationTests.java
+++ src/test/java/com/example/test_sp/TestSpApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.test;
+package com.example.test_sp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class TestSpApplicationTests {
 
     @Test
     void contextLoads() {
